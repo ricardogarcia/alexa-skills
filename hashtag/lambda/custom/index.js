5c56bd44d87fe25f8205ad4b19e7fcf216343e95
Ricardo Garcia <ricardo.garcia@alum.mit.edu>
2019-03-23 19:25:16 -0700

    remove sample comment

--- hashtag/lambda/custom/index.js
+++ hashtag/lambda/custom/index.js
@@ -1,6 +1,3 @@
-// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
-// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-// session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
 const Twitter = require('./twitter').Twitter;
 const twitter = new Twitter();
